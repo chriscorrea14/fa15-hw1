f63fe294a52f77692e4245fe120fd08bdd04d074
Chris Correa <chris.correa@berkeley.edu>
2015-09-18 18:19:03 +0000

    finished all

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,8 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+    a.map!{|x| x.to_i}
+    a.delete_if{|x| x % 2 != 0|| x > 8}
+    a.collect{|x| x+2}.reduce :+
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,29 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  return (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+#  for i in 1..(a.length-1)
+#    j = i
+#    puts("#{a[j]} #{a[i-1]}")
+#    while(a[j] < a[i-1] && j >= 0) do
+#      j = j - 1
+#    end
+#    tmp = a[j]
+#    a[j] = a[i]
+#    a[i] = tmp
+#  end
+#  puts a
+#  return a
+  a = a.sort
+  a.collect{ |x| x+1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
